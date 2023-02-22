012a40517af3e2c1f770a4e6eb8e17aed6fbccb3
developerHet <hetpatel2312.hp@gmail.com>
2023-02-22 19:03:01 +0530

    jwt

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+require("dotenv").config();
 const { Vonage } = require('@vonage/server-sdk')
 const { Text } = require('@vonage/messages/dist/classes/WhatsApp/Text');
+const jwt = require("jsonwebtoken");
+const sha256 = require('js-sha256');
 
 
 const app = express();
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 // Set up Nexmo/Vonage API credentials
@@ -19,42 +24,62 @@ const vonage = new Vonage({
   apiSecret: process.env.VONAGE_API_SECRET,
   applicationId: process.env.VONAGE_APPLICATION_ID,
     privateKey: __dirname +"/private.key"
-}, 
-//{
-  //apiHost: process.env.CYCLIC_URL
-//}
-)
+}
+);
 
 
 
 // Set up a webhook endpoint to receive GitHub push notifications
-app.post('/webhook', (req, res) => {
-  const payload = req.body;
-  const repoName = payload.repository.name;
-  const branchName = payload.ref.replace('refs/heads/', '');
-  const commitMsg = payload.head_commit.message;
+app.get('/webhook', (req, res) => {
+  // const payload = req.body;
+  // const repoName = payload.repository.name;
+  // const branchName = payload.ref.replace('refs/heads/', '');
+  // const commitMsg = payload.head_commit.message;
   
   // Compose a message to send via WhatsApp
-  const message = `New commit in ${repoName}/${branchName}: ${commitMsg}`;
+  //const message = `New commit in ${repoName}/${branchName}: ${commitMsg}`;
   
   // Use Nexmo/Vonage  to send the message via WhatsApp
-  vonage.channel.send(
-    {type:'whatsapp',number: process.env.TO_NUMBER},
-    {type:'whatsapp',number: process.env.WHATSAPP_NUMBER},
-    {
-      content: {
-        type: 'text',
-        text: message
-      }},(err) => {
-        if(err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-      }
-  );
+  vonage.messages.send(
+    new Text(
+      "Hello",
+      process.env.TO_NUMBER,
+      process.env.WHATSAPP_NUMBER
+    )
+  )
+    .then(resp => console.log(resp.message_uuid))
+    .catch(err => console.error(err));
   
   res.status(200).send('OK');
+});
+
+app
+    .route('/webhooks/inbound-message')    
+    .post(handleInboundMessage);
+
+function handleInboundMessage(request, response){
+  const payload = Object.assign(request.query, request.body)
+  let token = request.headers.authorization.split(" ")[1]
+  try{
+      var decoded = jwt.verify(token, VONAGE_API_SIGNATURE_SECRET, {algorithms:['HS256']});
+      if(sha256(JSON.stringify(payload))!=decoded["payload_hash"]){
+          console.log("tampering detected");
+          response.status(401).send();
+      }
+      else{
+          console.log("Success");
+          response.status(204).send();
+      }
+  }
+  catch(err){
+      console.log('Bad token detected')
+      response.status(401).send()
+  }
+}
+
+app.post('/webhooks/message-status', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
 });
 
 app.listen(port, () => {
